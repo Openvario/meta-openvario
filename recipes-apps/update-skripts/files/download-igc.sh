adf4da51e78297c529377be082eaf6700fb46aa1
Timo Bruderek <timo.bruderek@gmx.de>
2015-04-23 18:26:28 +0200

    Adapted path for IGC files

--- recipes-apps/update-skripts/files/download-igc.sh
+++ recipes-apps/update-skripts/files/download-igc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 USB_PATH="/usb/usbstick/openvario/igc"
-IGC_PATH="/root/"
-if [ -z "$(ls $MAP_PATH/*.igc 2>/dev/null)" ]; then
+IGC_PATH="/home/root/.xcsoar/logs"
+if [ -z "$(ls $IGC_PATH/*.igc 2>/dev/null)" ]; then
         echo "No files found !!!"
 else
-        for igcfile in $(find $MAP_PATH -name '*.igc'); do
+        for igcfile in $(find $IGC_PATH -name '*.igc'); do
                 echo $igcfile
                 cp $igcfile $USB_PATH
         done
