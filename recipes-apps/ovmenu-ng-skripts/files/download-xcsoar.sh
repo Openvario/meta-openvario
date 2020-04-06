5a91fd137cad1c7ef9810d0e977fbecfc3e9427b
bomilkar <ronald_niederhagen@freenet.de>
2020-04-06 10:08:15 +0200

    support all leagal file names and keep track of changes A simple "cp $downloadfile $USB_PATH" fails with blanks in file names (and other nasty chars). Use tar instead which can handle all legal file names and has 2 side effects: 1. keep multiple versions on the USB-stick 2. compress on the fly

--- recipes-apps/ovmenu-ng-skripts/files/download-xcsoar.sh
+++ recipes-apps/ovmenu-ng-skripts/files/download-xcsoar.sh
@@ -2,18 +2,19 @@
 
 USB_PATH="/usb/usbstick/openvario/download/xcsoar"
 DOWNLOAD_PATH="/home/root/.xcsoar"
+SAVE_FILE="${USB_PATH}/xcs-`date +'%Y-%m-%d'`.tgz"
 if [ ! -d "$USB_PATH" ]; then
 	mkdir "$USB_PATH"
 fi
+
 if [ -z "$(ls $DOWNLOAD_PATH/* 2>/dev/null)" ]; then
         echo "No files found !!!"
 else
-        for downloadfile in $(find $DOWNLOAD_PATH -type f); do
-                echo $downloadfile
-                cp $downloadfile $USB_PATH
-        done
+	cd ${DOWNLOAD_PATH}
+	tar czf "${SAVE_FILE}" .
 fi
 
+ls -l "${SAVE_FILE}"
 echo "Umount Stick ..."
 umount /dev/sda1
 echo "Done !!"
