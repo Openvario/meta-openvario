abf83f52b64c6debce3e5322e10e867d8adeda00
7lima <siebenlima@gmx.net>
2022-06-19 14:16:18 +0200

    Set --recursive rsync option: no need for trailing slash

--- meta-ov/recipes-apps/ovmenu-ng/files/backup-system.sh
+++ meta-ov/recipes-apps/ovmenu-ng/files/backup-system.sh
@@ -7,8 +7,8 @@ MAC=`ip li|fgrep ether|tail -1|cut -d ' ' -f 6| sed -e s/:/-/g`
 
 rsync --files-from - --archive --recursive --relative --mkpath --checksum \
 	/ "$USB_PATH/backup/$MAC"/ <<LISTE || exit $?
-/etc/dropbear/
-/etc/odpk/
+/etc/dropbear
+/etc/odpk
 /etc/locale.conf
 ...
 LISTE
