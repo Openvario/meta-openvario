f4aa4bcc641d49b98344c1868d9eb89acdf47482
7lima <siebenlima@gmx.net>
2022-06-19 14:01:37 +0200

    Simplify recursive copy operation by use of smart rsync features

--- meta-ov/recipes-apps/ovmenu-ng/files/backup-system.sh
+++ meta-ov/recipes-apps/ovmenu-ng/files/backup-system.sh
@@ -2,51 +2,16 @@
 
 # Transfer script for backup to usbstick
 
-USB_PATH=/usb/usbstick/openvario/backup
+USB_PATH=/usb/usbstick/openvario
 MAC=`ip li|fgrep ether|tail -1|cut -d ' ' -f 6`
 
-if mkdir -p "$USB_PATH/$MAC"
-then :;	else
-	echo "Can't create directory '$USB_PATH/$MAC'"
-	exit 100
-fi
-
-echo "
+rsync --files-from - --archive --relative --mkpath --checksum \
+	/ "$USB_PATH/backup/$MAC"/ <<LISTE || exit $?
 /etc/dropbear
 /etc/odpk
+/etc/locale.conf
 /var/lib/...
-" | grep -P -v "^[\t\n\r ]*$" | 
-while read DIR
-do
-	echo "Path: $DIR"
-	SRC_PATH="$DIR"
-	DEST_PATH="$USB_PATH/$MAC/$DIR"
-
-	if mkdir -p "$DEST_PATH"
-	then :;	else
-		echo "Can't create directory '$DEST_PATH'"
-		exit 102
-	fi
-
-	if [ ! -d "$SRC_PATH" ] || [ ! -d "$DEST_PATH" ]; then
-		>&2 echo "Source $SRC_PATH or destination path $DEST_PATH does not exist"
-		exit 101
-	fi
-	
-	if [ -z "$(find "$SRC_PATH" -type f | head -n 1 2>/dev/null)" ]; then
-		echo 'No files found !!!'
-	fi
-	
-	# We use -c here due to cubieboards not having an rtc clock
-#	if rsync -r -c --progress "$SRC_PATH/" "$DEST_PATH/"; then
-	if rsync --dry-run -r -c --progress "$SRC_PATH/" "$DEST_PATH/"; then
-		echo 'All files transfered successfully.'
-	else
-		EXIT=$?
-		>&2 echo 'An error has occured!'
-		exit $EXIT
-	fi
-done
+LISTE
 
 # Sync the buffer to be sure data is on disk
 sync
