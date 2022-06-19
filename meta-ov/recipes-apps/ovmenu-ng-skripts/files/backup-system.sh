93da2db87e9e9b9d1520e4643d19166bda45ca3e
Blaubartgmx.de <Blaubart@gmx.de>
2022-06-19 21:53:42 +0200

    make sure MAC address is used by eth0

--- meta-ov/recipes-apps/ovmenu-ng-skripts/files/backup-system.sh
+++ meta-ov/recipes-apps/ovmenu-ng-skripts/files/backup-system.sh
@@ -6,14 +6,17 @@
 # Path where the USB stick is mounted
 USB_PATH=/usb/usbstick/openvario
 # MAC address of the first Ethernet device to do a separate backup
-MAC=`ip li|fgrep ether|head -n 1|cut -d ' ' -f 6|sed -e s/:/-/g`
+MAC=`ip li|fgrep -A 1 eth0|head -n 2|cut -d ' ' -f 6|tail -n 1|sed -e s/:/-/g`
 
 # Copy all directories and files from (example) list below to backup directory
 echo "/etc/dropbear
-/etc/odpk
+/etc/opkg
 /etc/locale.conf
-...
-/adapt/to/your/needs" |
+/home/root
+/opt/conf
+/var/lib/connman
+/boot/config.uEnv
+/sys/class/backlight/lcd/brightness" |
 rsync --files-from - -v --archive --recursive --relative --mkpath --checksum \
 	/ "$USB_PATH/backup/$MAC"/ || exit $?
 
@@ -29,4 +32,4 @@ rsync --files-from - -v --archive --recursive --relative --mkpath --checksum \
 
 # Sync the buffer to be sure data is on disk
 sync
-echo Done '!!'
+echo Done '!!' 
--- meta-ov/recipes-apps/ovmenu-ng-skripts/files/restore-system.sh
+++ meta-ov/recipes-apps/ovmenu-ng-skripts/files/restore-system.sh
@@ -3,11 +3,11 @@
 # Transfer script for restore from usbstick
 
 USB_PATH=/usb/usbstick/openvario/backup
-MAC=`ip li|fgrep ether|tail -1|cut -d ' ' -f 6`
+MAC=`ip li|fgrep -A 1 eth0|head -n 2|cut -d ' ' -f 6|tail -n 1|sed -e s/:/-/g`
 
 # We use -c here due to cubieboards not having an rtc clock
-#if rsync -r -c --progress "$USB_PATH/" /; then
-if rsync --dry-run -r -c --progress "$USB_PATH/$MAC/" /; then
+if rsync -r -c --progress "$USB_PATH/$MAC/" /; then
+#if rsync --dry-run -r -c --progress "$USB_PATH/$MAC/" /; then
 	echo 'All files transfered successfully.'
 else
 	EXIT=$?
