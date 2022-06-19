7936e8ca8119262b7057e0e0f2a35c5313983029
7lima <siebenlima@gmx.net>
2022-06-19 14:32:13 +0200

    Use head instead of tail. Comments!

--- meta-ov/recipes-apps/ovmenu-ng/files/backup-system.sh
+++ meta-ov/recipes-apps/ovmenu-ng/files/backup-system.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
+#
+# Transfer script for OpenVario backup to usbstick
+# 7lima, 2022-06-19
 
-# Transfer script for backup to usbstick
-
+# Path where the USB stick is mounted
 USB_PATH=/usb/usbstick/openvario
-MAC=`ip li|fgrep ether|tail -1|cut -d ' ' -f 6| sed -e s/:/-/g`
+# MAC address of the first Ethernet device to do a separate backup
+MAC=`ip li|fgrep ether|head -n 1|cut -d ' ' -f 6|sed -e s/:/-/g`
 
+# Copy all directories and files from (example) list below to backup directory
 rsync --files-from - --archive --recursive --relative --mkpath --checksum \
-	/ "$USB_PATH/backup/$MAC"/ <<LISTE || exit $?
+	/ "$USB_PATH/backup/$MAC"/ <<LIST || exit $?
 /etc/dropbear
 /etc/odpk
 /etc/locale.conf
 ...
-LISTE
+/adapt/to/your/needs
+LIST
 
 # Sync the buffer to be sure data is on disk
 sync
-echo 'Done !!'
+echo Done '!!'
