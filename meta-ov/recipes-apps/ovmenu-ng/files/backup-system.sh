76a2677598d748101ea0779c9d21c5b087f16716
7lima <siebenlima@gmx.net>
2022-06-19 14:11:37 +0200

    Set --recursive rsync option

--- meta-ov/recipes-apps/ovmenu-ng/files/backup-system.sh
+++ meta-ov/recipes-apps/ovmenu-ng/files/backup-system.sh
@@ -5,12 +5,12 @@
 USB_PATH=/usb/usbstick/openvario
 MAC=`ip li|fgrep ether|tail -1|cut -d ' ' -f 6| sed -e s/:/-/g`
 
-rsync --files-from - --archive --relative --mkpath --checksum \
+rsync --files-from - --archive --recursive --relative --mkpath --checksum \
 	/ "$USB_PATH/backup/$MAC"/ <<LISTE || exit $?
-/etc/dropbear
-/etc/odpk
+/etc/dropbear/
+/etc/odpk/
 /etc/locale.conf
-/var/lib/...
+...
 LISTE
 
 # Sync the buffer to be sure data is on disk
