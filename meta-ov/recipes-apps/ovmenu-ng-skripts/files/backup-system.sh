98f26a422ffbc76a4c9fb663e4531b877a00d225
7lima <20513943+7lima@users.noreply.github.com>
2022-06-25 14:35:50 +0200

    Straightlined and refactored backup/restore with Blaubart's changes

--- meta-ov/recipes-apps/ovmenu-ng-skripts/files/backup-system.sh
+++ meta-ov/recipes-apps/ovmenu-ng-skripts/files/backup-system.sh
@@ -7,29 +7,23 @@
 USB_PATH=/usb/usbstick/openvario
 
 # MAC address of the Ethernet device eth0 to do a separate backup
-MAC=`ip li|fgrep -A 1 eth0|head -n 2|cut -d ' ' -f 6|tail -n 1|sed -e s/:/-/g`
+MAC=`ip li|fgrep -A 1 eth0|tail -n 1|cut -d ' ' -f 6|sed -e s/:/-/g`
 
 # Store SSH-status 
-if /bin/systemctl --quiet is-enabled dropbear.socket; then
-	>/home/root/ssh-status
-	echo enabled >> /home/root/ssh-status
-	
-elif /bin/systemctl --quiet is-active dropbear.socket; then
-	>/home/root/ssh-status
-	echo temporary >> /home/root/ssh-status
-	
+if   /bin/systemctl --quiet is-enabled dropbear.socket; then
+	echo enabled
+elif /bin/systemctl --quiet is-active  dropbear.socket; then
+	echo temporary
 else 
-	>/home/root/ssh-status
-	echo disabled >> /home/root/ssh-status
-	
-fi
+	echo disabled  
+fi > /home/root/ssh-status
 
 # Copy brightness setting
->/home/root/brightness
-cat /sys/class/backlight/lcd/brightness >> /home/root/brightness
+cat /sys/class/backlight/lcd/brightness > /home/root/brightness
 
 # Copy all directories and files from list below to backup directory
-echo "/etc/locale.conf
+echo "
+/etc/locale.conf
 /etc/udev/rules.d/libinput-ts.rules
 /etc/pointercal
 /etc/dropbear
@@ -39,16 +33,20 @@ echo "/etc/locale.conf
 /var/lib/connman
 /boot/config.uEnv
 " |
-if rsync --files-from - -v --archive --recursive --quiet --relative --mkpath --checksum --safe-links\
-	/ "$USB_PATH/backup/$MAC"/ || exit $?; then
-	echo -e ' All files have been backed up. \n Wait until "Done !!" appears before you exit! \n'
-
-else
+if 
+# Copy all files and dirs recursively
+	rsync --files-from - --archive --recursive --quiet --relative --mkpath --checksum --safe-links \
+		/ "$USB_PATH/backup/$MAC"/
 	EXIT=$?
-	>&2 echo ' An error has occured!'
-	exit $EXIT
+	echo ' Wait until "Done !!" appears before you exit!'
+# Sync the buffer to be sure data is on disk
+	sync
+	test $EXIT -eq 0
+then
+	echo ' All files have been backed up.'
+else 
+	echo ' An error has occured!'
 fi
 
-# Sync the buffer to be sure data is on disk
-sync
 echo ' Done !!' 
+exit $EXIT
--- meta-ov/recipes-apps/ovmenu-ng-skripts/files/restore-system.sh
+++ meta-ov/recipes-apps/ovmenu-ng-skripts/files/restore-system.sh
@@ -7,36 +7,41 @@
 USB_PATH=/usb/usbstick/openvario/backup
 
 # MAC address of the Ethernet device eth0 to restore the separate backup
-MAC=`ip li|fgrep -A 1 eth0|head -n 2|cut -d ' ' -f 6|tail -n 1|sed -e s/:/-/g`
+MAC=`ip li|fgrep -A 1 eth0|tail -n 1|cut -d ' ' -f 6|sed -e s/:/-/g`
 
 # Restore SSH-status 
-SSH=$(cat $USB_PATH/$MAC/home/root/ssh-status)
-if [ "$SSH" = "enabled" ]; then
+SSH=`cat $USB_PATH/$MAC/home/root/ssh-status`
+if [ "$SSH" = enabled ]; then
 	echo " SSH has been enabled permanently."
-	/bin/systemctl enable --quiet  --now dropbear.socket
+	/bin/systemctl enable  --quiet --now dropbear.socket
 	
-elif [ "$SSH" = "temporary" ]; then
+elif [ "$SSH" = temporary ]; then
 	echo " SSH has been enabled temporarily."
 	/bin/systemctl disable dropbear.socket
-	/bin/systemctl start dropbear.socket
+	/bin/systemctl start   dropbear.socket
 	
-elif [ "$SSH" = "disabled" ]; then
+elif [ "$SSH" = disabled ]; then
 	echo " SSH has been disabled."
 	/bin/systemctl disable --quiet --now dropbear.socket
 fi
 
 # Restore brightness setting
-cat $USB_PATH/$MAC/home/root/brightness >> /sys/class/backlight/lcd/brightness
+cat $USB_PATH/$MAC/home/root/brightness > /sys/class/backlight/lcd/brightness
 
+if 
+# Copy all files and dirs recursively.
 # We use -c here due to cubieboards not having an rtc clock
-if rsync -r -c --progress --quiet "$USB_PATH/$MAC/" /; then
-	echo -e ' All files have been restored. \n Wait until "Done !!" appears before you exit! \n'
-else
+	rsync --recursive --mkpath --checksum --quiet "$USB_PATH/$MAC/" /
 	EXIT=$?
-	>&2 echo ' An error has occured!'
-	exit $EXIT
+	echo ' Wait until "Done !!" appears before you exit!'
+# Sync the buffer to be sure data is on disk
+	sync
+	test $EXIT -eq 0
+then
+	echo ' All files have been restored.'
+else 
+	echo ' An error has occured!'
 fi
 
-# Sync the buffer to be sure data is on disk
-sync
-echo ' Done !!'
+echo ' Done !!' 
+exit $EXIT
