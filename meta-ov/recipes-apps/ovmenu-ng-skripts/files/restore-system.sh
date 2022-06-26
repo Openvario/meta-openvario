a776880310039cc5c0d3d58948457f498634a747
7lima <siebenlima@gmx.net>
2022-06-26 16:24:10 +0200

    Take values from files after they have been restored to home directory

--- meta-ov/recipes-apps/ovmenu-ng-skripts/files/restore-system.sh
+++ meta-ov/recipes-apps/ovmenu-ng-skripts/files/restore-system.sh
@@ -10,45 +10,6 @@ BACKUP=openvario/backup
 # MAC address of the Ethernet device eth0 to restore the separate backup
 MAC=`ip li|fgrep -A 1 eth0|tail -n 1|cut -d ' ' -f 6|sed -e s/:/-/g`
 
-# Restore SSH-status 
-SSH=`cat "$USB_PATH/$BACKUP/$MAC"/home/root/ssh-status`
-if [ "$SSH" = enabled ]; then
-	echo " SSH has been enabled permanently."
-	/bin/systemctl enable  --quiet --now dropbear.socket
-	
-elif [ "$SSH" = temporary ]; then
-	echo " SSH has been enabled temporarily."
-	/bin/systemctl disable dropbear.socket
-	/bin/systemctl start   dropbear.socket
-	
-elif [ "$SSH" = disabled ]; then
-	echo " SSH has been disabled."
-	/bin/systemctl disable --quiet --now dropbear.socket
-fi
-
-# Restore variod-status 
-variod=`cat "$USB_PATH/$BACKUP/$MAC"/home/root/variod-status`
-if [ "$variod" = enabled ]; then
-	echo " variod has been enabled."
-	/bin/systemctl enable  --quiet --now variod
-elif [ "$variod" = disabled ]; then
-	echo " variod has been disabled."
-	/bin/systemctl disable --quiet --now variod
-fi
-
-# Restore sensord-status 
-sensord=`cat "$USB_PATH/$BACKUP/$MAC"/home/root/sensord-status`
-if [ "$sensord" = enabled ]; then
-	echo " sensord has been enabled."
-	/bin/systemctl enable  --quiet --now sensord
-elif [ "$sensord" = disabled ]; then
-	echo " sensord has been disabled."
-	/bin/systemctl disable --quiet --now sensord
-fi
-
-# Restore brightness setting
-cat "$USB_PATH/$BACKUP/$MAC"/home/root/brightness > /sys/class/backlight/lcd/brightness
-
 if 
 # Copy all files and dirs recursively.
 # We use -c here due to cubieboards not having an rtc clock
@@ -64,6 +25,45 @@ then
 else 
 	echo ' An error has occured!'
 fi
+
+# Restore SSH-status 
+SSH=`cat /home/root/ssh-status`
+if [ "$SSH" = enabled ]; then
+	echo " SSH has been enabled permanently."
+	/bin/systemctl enable  --quiet --now dropbear.socket
+	
+elif [ "$SSH" = temporary ]; then
+	echo " SSH has been enabled temporarily."
+	/bin/systemctl disable dropbear.socket
+	/bin/systemctl start   dropbear.socket
+	
+elif [ "$SSH" = disabled ]; then
+	echo " SSH has been disabled."
+	/bin/systemctl disable --quiet --now dropbear.socket
+fi
+
+# Restore variod-status 
+variod=`cat /home/root/variod-status`
+if [ "$variod" = enabled ]; then
+	echo " variod has been enabled."
+	/bin/systemctl enable  --quiet --now variod
+elif [ "$variod" = disabled ]; then
+	echo " variod has been disabled."
+	/bin/systemctl disable --quiet --now variod
+fi
+
+# Restore sensord-status 
+sensord=`cat /home/root/sensord-status`
+if [ "$sensord" = enabled ]; then
+	echo " sensord has been enabled."
+	/bin/systemctl enable  --quiet --now sensord
+elif [ "$sensord" = disabled ]; then
+	echo " sensord has been disabled."
+	/bin/systemctl disable --quiet --now sensord
+fi
+
+# Restore brightness setting
+cat /home/root/brightness > /sys/class/backlight/lcd/brightness
 
 echo ' Done !!' 
 exit $EXIT
