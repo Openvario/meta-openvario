33dbddfe428158a468ca92f46b94f7a5bf39acb0
7lima <20513943+7lima@users.noreply.github.com>
2022-07-17 01:05:42 +0200

    Moved system info functionality into new script system-info.sh. Extensions
    System info script gives an overview which packages and which versions are installed. It also shows IP addresses, kernel, host, system daemon status and other info.
    Moved functional code from menu ovmenu-ng.sh into file system-info.sh.
    A bunch of new system information was added

--- meta-ov/recipes-apps/ovmenu-ng/files/ovmenu-ng.sh
+++ meta-ov/recipes-apps/ovmenu-ng/files/ovmenu-ng.sh
@@ -100,30 +100,9 @@ function submenu_system() {
 	esac
 }
 
-function show_info() {
-	### collect info of system
-	XCSOAR_VERSION=$(opkg list-installed xcsoar | awk -F' ' '{print $3}')
-	XCSOAR_MAPS_VERSION=$(opkg list-installed | grep "xcsoar-maps-" | awk -F' ' '{print $3}')
-	IMAGE_VERSION=$(cat /etc/os-release | grep VERSION_ID | awk -F'=' -F'"' '{print $2}')
-	SENSORD_VERSION=$(opkg list-installed sensord* | awk -F' ' '{print $3}')
-	VARIOD_VERSION=$(opkg list-installed variod* | awk -F' ' '{print $3}')
-	IP_ETH0=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-	IP_WLAN=$(/sbin/ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-
-	dialog --backtitle "OpenVario" \
-	--title "[ S Y S T E M I N F O ]" \
-	--begin 3 4 \
-	--msgbox " \
-	\n \
-	Image: $IMAGE_VERSION\n \
-	XCSoar: $XCSOAR_VERSION\n \
-	Maps: $XCSOAR_MAPS_VERSION\n \
-	sensord: $SENSORD_VERSION\n \
-	variod: $VARIOD_VERSION\n \
-	IP eth0: $IP_ETH0\n \
-	IP wlan0: $IP_WLAN\n \
-	" 15 50
-
+function show_info() { 
+	/usr/bin/system-info.sh >> /tmp/tail.$$ &
+	dialog --backtitle "OpenVario" --title "Result" --tailbox /tmp/tail.$$ 30 50
 }
 
 function submenu_settings() {
