2dd5b822cbe3723084d499bb672db0e19e7e7240
Stefan Schumann <stefan.schumann@posteo.de>
2026-04-09 19:38:56 +0200

    ovmenu-ng-opensoar: remove init logic now handled by ov-system-init
    Brightness setting, upgrade config restoration, data partition creation and mounting are already handled by the ov-system-init service that runs before ovmenu-ng. Remove the duplicated logic from the OpenSoar menu script.
    
    The script still works as a standalone entry-point: it sources config.uEnv (for main_app), sets up DATADIR, and runs the main app / exit-code loop.

--- recipes-apps/ovmenu-ng/files/ovmenu-ng-opensoar.sh
+++ recipes-apps/ovmenu-ng/files/ovmenu-ng-opensoar.sh
@@ -4,7 +4,6 @@ DEBUG_STOP=No
 
 INPUT=/tmp/menu.sh.$$
 DATADIR=$HOME/data
-RECOVER_DIR=$HOME/recover_data
 
 # trap and delete temp files
 trap "rm $INPUT;rm /tmp/tail.$$; exit" SIGHUP SIGINT SIGTERM
@@ -26,64 +25,8 @@ function debug_stop() {
 sleep 1
 #=========================================================================
 source /boot/config.uEnv
-if [[ -z "$brightness" ]]; then 
-  brightness=10
-fi
-echo "$brightness" >/sys/class/backlight/lcd/brightness
-debug_stop "set brightness to '$brightness'"
 
-#------------------------------------------------------------------------------
-# set system configs if upgrade.cfg is available (from Upgrade)
 cd $HOME
-if [ -f $RECOVER_DIR/upgrade.cfg ]; then
-  echo "Update system config" > sysconfig.txt
-  /usr/bin/update-system-config.sh
-elif [ ! -f $RECOVER_DIR/_upgrade.cfg ]; then
-  echo "upgrade.cfg not found" > sysconfig.txt
-else
-  echo "only backup config found !!!!!" > sysconfig.txt
-fi
-
-#------------------------------------------------------------------------------
-echo "begin startup.. $(date "+%Y-%m-%d %H:%M:%S")" >> $DEBUG_LOG
-DATESTRING=$(date "+%Y-%m-%d %H:%M:%S")
-date "+%Y-%m-%d %H:%M:%S" >> $DEBUG_LOG
-echo "=================================" >> $DEBUG_LOG
-if [ ! -e /dev/mmcblk0p3 ]; then
-  echo "/dev/mmcblk0p3 don't exist " >> $DEBUG_LOG
-  ls -l /dev/mm* >> $DEBUG_LOG
-  # create the 3rd SD card partition:
-  source /usr/bin/create_datapart.sh
-
-  echo "Debug: 3rd SD card partition created" >> $DEBUG_LOG
-  if [ -f $HOME/_upgrade.cfg ]; then
-    # reactivate the previous system data
-    mv -f $HOME/_upgrade.cfg $HOME/upgrade.cfg
-  fi 
-      ### read -p "Press enter to continue"
-  if [ ! -e /dev/mmcblk0p3 ]; then
-    echo "Reboot ====================================" >> $DEBUG_LOG
-    echo "Wait until OpenVario after Reboot is ready!" >> $DEBUG_LOG
-    reboot
-    ### read -p "Press enter to continue"
-  fi
-fi
-
-#------------------------------------------------------------------------------
-# Mount the 3rd partition to the data dir
-mount /dev/mmcblk0p3 $DATADIR
-if [ ! -d $DATADIR/OpenSoarData ]; then
-  if ! mount /dev/mmcblk0p3 $DATADIR; then
-    if ! mkfs.ext4 /dev/mmcblk0p3; then
-      echo "Error 1: mmcblk0p3 couldn't be formatted"  >> $DEBUG_LOG
-    fi
-    if ! mount /dev/mmcblk0p3 $DATADIR; then
-      echo "Error 2: mmcblk0p3 couldn't be mounted"  >> $DEBUG_LOG
-    fi
-  fi
-else
-  echo "mmcblk0p3 is mounted at '$DATADIR'"  >> $DEBUG_LOG
-fi
 
 #------------------------------------------------------------------------------
 function do_shell() {
